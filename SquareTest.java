6b4982223eee78521a21bf87c1b2c0e1c6822841
nipuni-sathsarani <79597637+nipuni-sathsarani@users.noreply.github.com>
2022-06-19 21:30:52 +0530

    added SquareTest.java

---
+++ SquareTest.java
@@ -0,0 +1,20 @@
+package Square;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+public class SquareTest {
+	@Test
+	public void testLength() {
+		Square r = new Square(5,5);	
+		assertEquals(3,r.getLength());
+	}
+	@Test
+	public void testWidth() {
+		Square r = new Square(5,5);	
+		assertEquals(5,r.getWidth());
+	}
+	@Test
+	public void testArea() {
+		Square r = new Square(5,5);
+		assertEquals(25,r.getArea());
+	}
+}
