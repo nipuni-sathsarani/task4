488b5c50311e98c2d23fa833a18e281f27587e65
nipuni-sathsarani <79597637+nipuni-sathsarani@users.noreply.github.com>
2022-06-19 21:29:45 +0530

    added Reflection10.java

---
+++ Reflection10.java
@@ -0,0 +1,12 @@
+package Square;
+import java.lang.reflect.*;
+
+public class Reflection10 {
+	public static void main(String[] args) throws Exception {
+		Square s = new Square(10,20);
+		Method m = s.getClass().getDeclaredMethod("setLength", int.class);
+		m.setAccessible(true);
+		m.invoke(s, 86);
+		System.out.println(s);
+	}
+}
